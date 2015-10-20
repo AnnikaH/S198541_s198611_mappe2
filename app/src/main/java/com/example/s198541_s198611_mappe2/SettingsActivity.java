54e8fa866afe3a3228082a099840a4c4132889bb
AnnikaH <annika_limfe@hotmail.com>
2015-10-20 19:46:15 +0200

    SettingsActivity and related files/code added

---
+++ app/src/main/java/com/example/s198541_s198611_mappe2/SettingsActivity.java
@@ -0,0 +1,40 @@
+package com.example.s198541_s198611_mappe2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new PrefsFragment()).commit();
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+            Preference langPref = findPreference("change_language");
+            langPref.setSummary(Locale.getDefault().getDisplayLanguage());
+            langPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent i = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                    startActivity(i);
+                    return true;
+                }
+            });
+        }
+    }
+}
