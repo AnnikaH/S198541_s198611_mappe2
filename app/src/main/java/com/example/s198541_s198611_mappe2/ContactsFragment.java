43473c92627080b61d4c7cb6e92a3dafd4749a8c
AnnikaH <annika_limfe@hotmail.com>
2015-10-21 05:29:58 +0200

    Removed commented out code that we're not going to use

--- app/src/main/java/com/example/s198541_s198611_mappe2/ContactsFragment.java
+++ app/src/main/java/com/example/s198541_s198611_mappe2/ContactsFragment.java
@@ -53,23 +53,7 @@ public class ContactsFragment extends Fragment implements LoaderManager.LoaderCa
 
         dbHandler = new DBHandler(getActivity().getBaseContext());
 
-        // List<Person> persons = dbHandler.getAllPersons();
-
-        /*String[] personNameArray = new String[persons.size()];
-
-        int count = 0;
-        for(Person pers : persons) {
-            String log = "Id: " + pers.get_ID() + ", Name: " +
-                    pers.getName() + ", PhoneNumber: " + pers.getPhoneNumber() +
-                    ", Birthday: " + pers.getBirthday() + ", Message is null for now";
-
-            personNameArray[count++] = pers.getName() + " " + pers.getBirthday();
-            Log.d("Persons: ", log);
-        }*/
-
-        //String[] uiBindFrom = {ContactsContract.Contacts.DISPLAY_NAME};
         String[] uiBindFrom = { DBHandler.KEY_NAME };
-
         int[] uiBindTo = { android.R.id.text1 };
 
         mAdapter = new SimpleCursorAdapter(getActivity().getBaseContext(),
@@ -79,6 +63,7 @@ public class ContactsFragment extends Fragment implements LoaderManager.LoaderCa
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Go to editing (or deleting) this person
 
                 int idPerson = (int) id;
 
@@ -103,7 +88,7 @@ public class ContactsFragment extends Fragment implements LoaderManager.LoaderCa
 
         searchView.setOnQueryTextListener(this);
 
-        loaderManager.initLoader(0, null, this);    // 0 er id'en
+        loaderManager.initLoader(0, null, this);
     }
 
     public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
@@ -145,139 +130,4 @@ public class ContactsFragment extends Fragment implements LoaderManager.LoaderCa
     public boolean onQueryTextSubmit(String query) {
         return false;
     }
-
-/*
-    // Når trykker på Legg til-knappen
-    public void leggTil(View view) {
-        EditText navnefelt = (EditText) findViewById(R.id.navn);
-        EditText telefonfelt = (EditText) findViewById(R.id.telefon);
-
-        String navn = navnefelt.getText().toString();
-        String telefon = telefonfelt.getText().toString();
-
-        // Teste på om telefonnummeret finnes i kontaktene fra før?
-
-        if(!navn.equals("") && !telefon.equals("")) {
-            Kontakt kontakt = new Kontakt(navn, telefon);
-            db.leggTilKontakt(kontakt);
-
-            // Egentlig: dialogboks/pop-up med beskjed
-//            Kontakt k = db.finnKontakt(kontakt.get_ID());
-//            Log.d("LEGG TIL: ", "Kontakt lagt til! Navn: " + k.getNavn());
-
-            List<Kontakt> kontakter = db.finnAlleKontakter();
-            for(Kontakt kont : kontakter) {
-                String log = "Id: " + kont.get_ID() + ", Navn: " +
-                        kont.getNavn() + ", Telefon: " + kont.getTelefon();
-                Log.d("Navn: ", log);
-            }
-        }
-        else {
-            // Egentlig: dialogboks/pop-up med beskjed
-            Log.d("LEGG TIL: ", "Både navn og telefonnummer må fylles inn!");
-        }
-    }
-
-    // Når trykker på Slett-knappen
-    public void slett(View view) {
-        String telefon = ((EditText) findViewById(R.id.telefon_slette)).getText().toString();
-
-        Kontakt k = db.finnKontakt(telefon);
-
-        if(k != null) {
-            db.slettKontakt(k);
-
-            Log.d("SLETTE: ", "Kontakt slettet!");
-
-            List<Kontakt> kontakter = db.finnAlleKontakter();
-            for(Kontakt kont : kontakter) {
-                String log = "Id: " + kont.get_ID() + ", Navn: " +
-                        kont.getNavn() + ", Telefon: " + kont.getTelefon();
-                Log.d("Navn: ", log);
-            }
-        } else {
-            Log.d("SLETTE: ", "Kontakt med telefonnr. ikke funnet!");
-        }
-    }
-
-    // TODO: HAR IKKE TESTET DENNE!
-    // Når trykker på Endre-knappen:
-    public void endre(View view) {
-        // ev.: EditText navnefeltFra = (EditText) findViewById(R.id.navn_endre_fra);
-
-        EditText telefonfeltFra = (EditText) findViewById(R.id.telefon_endre_fra);
-        String telefon = telefonfeltFra.getText().toString();
-
-        Kontakt k = db.finnKontakt(telefon);
-
-        if(k != null) {
-            EditText navnefeltTil = (EditText) findViewById(R.id.navn_endre_til);
-            EditText telefonfeltTil = (EditText) findViewById(R.id.telefon_endre_til);
-
-            String nyttNavn = navnefeltTil.toString();
-            String nyttTelefonnr = telefonfeltTil.toString();
-
-            if(!nyttNavn.equals("")) {
-                k.setNavn(navnefeltTil.toString());
-            }
-
-            // også teste om er bare tall (om Integer.parseInt(..) er mulig) (try catch)
-            if(!nyttTelefonnr.equals("")) {
-                k.setTelefon(telefonfeltTil.toString());
-            }
-
-            db.oppdaterKontakt(k);
-
-            listAlle();
-        } else {
-            Log.d("ENDRE: ", "Fant ikke kontakt med dette telefonnummeret!");
-        }
-    }
-
-    // TODO: HAR IKKE TESTET DENNE!
-    // Når trykker på Finn-knappen:
-    public void finn(View view) {
-        EditText navnefelt = (EditText) findViewById(R.id.navn_finne);
-        EditText telefonfelt = (EditText) findViewById(R.id.telefon_finne);
-
-        String navn = navnefelt.getText().toString();
-        String telefon = telefonfelt.getText().toString();
-
-        Kontakt k;
-
-        // Et alternativ (dekker ikke alle muligheter):
-        if(!navn.equals("")) {
-            k = db.finnKontakt(navn);
-        } else if(!telefon.equals("")) {
-            k = db.finnKontakt(telefon);
-        } else {
-            k = null;
-        }
-
-        if(k != null) {
-            Log.d("FINNE: ", "Fant en kontakt!");
-
-            TextView tekstomraade = (TextView) findViewById(R.id.list_alle_tekstomraade);
-            tekstomraade.setText("");   // clear
-
-            tekstomraade.setText("Navn: " + k.getNavn() + " Telefon: " + k.getTelefon());
-        } else {
-            Log.d("FINNE: ", "Fant ingen kontakter som matcher dette navnet (ev. tlfnr. hvis navn tomt)!");
-        }
-    }
-
-    // Når trykker på List alle-knappen:
-    public void listAlle() {
-        TextView tekstomraade = (TextView) findViewById(R.id.list_alle_tekstomraade);
-        tekstomraade.setText("");   // clear
-
-        List<Kontakt> alleKontakter = db.finnAlleKontakter();
-
-        for(Kontakt kont : alleKontakter) {
-            tekstomraade.append("Navn: " + kont.getNavn() + " Telefon: " + kont.getTelefon() + "\n");
-        }
-
-        // også håndtere hvis er ingen kontakter
-    }
-*/
 }
