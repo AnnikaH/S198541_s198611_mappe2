48c6cf482b41432e0f29ad2a9998bf71d89c10b4
AnnikaH <annika_limfe@hotmail.com>
2015-10-22 23:07:24 +0200

    Working on sms service

--- app/src/main/java/com/example/s198541_s198611_mappe2/ResultOfNotificationActivity.java
+++ app/src/main/java/com/example/s198541_s198611_mappe2/ResultOfNotificationActivity.java
@@ -13,14 +13,12 @@ import java.util.List;
 
 public class ResultOfNotificationActivity extends AppCompatActivity {
 
-    private DBHandler dbHandler;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_of_notification);
 
-        dbHandler = new DBHandler(this);
+        DBHandler dbHandler = new DBHandler(this);
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.sms_information_layout);
 
@@ -41,27 +39,5 @@ public class ResultOfNotificationActivity extends AppCompatActivity {
                 layout.addView(text);
             }
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_birthday_service_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-      /*  if (id == R.id.action_settings) {
-            return true;
-        }*/
-
-        return super.onOptionsItemSelected(item);
     }
 }
