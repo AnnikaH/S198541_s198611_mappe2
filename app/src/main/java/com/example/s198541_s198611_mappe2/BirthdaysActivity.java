bf564f4913f0bc085974eb8b81fe6ac3c55b1ea5
AnnikaH <annika_limfe@hotmail.com>
2015-10-23 02:50:32 +0200

    Working on birthday list

--- app/src/main/java/com/example/s198541_s198611_mappe2/BirthdaysActivity.java
+++ app/src/main/java/com/example/s198541_s198611_mappe2/BirthdaysActivity.java
@@ -10,28 +10,17 @@ public class BirthdaysActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_birthday_list);
-    }
+        setContentView(R.layout.activity_birthdays);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_birthday_service_main, menu);
-        return true;
-    }
+        if(findViewById(R.id.fragment_container) != null) {
+            if(savedInstanceState != null) {
+                return;
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-      /*  if (id == R.id.action_settings) {
-            return true;
-        }*/
-
-        return super.onOptionsItemSelected(item);
+            BirthdaysFragment birthdaysFragment = new BirthdaysFragment();
+            birthdaysFragment.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, birthdaysFragment).commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/s198541_s198611_mappe2/BirthdaysFragment.java
@@ -0,0 +1,74 @@
+package com.example.s198541_s198611_mappe2;
+
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class BirthdaysFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    LoaderManager loaderManager;
+    CursorLoader cursorLoader;
+    SimpleCursorAdapter mAdapter;
+    DBHandler dbHandler;
+    String TAG = "LOADER";
+    ListView listView;
+
+    public BirthdaysFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.list_birthdays, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        loaderManager = getActivity().getLoaderManager();
+        dbHandler = new DBHandler(getActivity().getBaseContext());
+
+        String[] uiBindFrom = { DBHandler.KEY_BIRTHDAY, DBHandler.KEY_NAME };
+        int[] uiBindTo = { android.R.id.text1, android.R.id.text2 };
+
+        mAdapter = new SimpleCursorAdapter(getActivity().getBaseContext(),
+                android.R.layout.simple_list_item_2, null, uiBindFrom, uiBindTo, 0);
+        listView = (ListView) getActivity().findViewById(R.id.list_view);
+        listView.setAdapter(mAdapter);
+        loaderManager.initLoader(0, null, this);
+    }
+
+    public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+        String[] projection = { DBHandler.KEY_ID, DBHandler.KEY_NAME, DBHandler.KEY_BIRTHDAY } ;
+        cursorLoader = new CursorLoader(getActivity().getBaseContext(), PersonCP.CONTENT_URI, projection,
+                null, null, DBHandler.KEY_BIRTHDAY);
+        return cursorLoader;
+    }
+
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        if(mAdapter != null && cursor != null)
+            mAdapter.swapCursor(cursor);
+            // swap the new cursor in
+        else
+            Log.v(TAG, "OnLoadFinished: mAdapter is null");
+    }
+
+    public void onLoaderReset(Loader<Cursor> arg0) {
+        if(mAdapter != null)
+            mAdapter.swapCursor(null);
+        else
+            Log.v(TAG, "OnLoadFinished: mAdapter is null");
+    }
+}
