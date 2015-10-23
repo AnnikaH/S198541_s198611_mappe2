57a736b15cfb4b0c94202b7abfdb244641fb10c0
AnnikaH <annika_limfe@hotmail.com>
2015-10-23 03:52:09 +0200

    Some changes to Change message activity - save not storing in database - don't know why

--- app/src/main/java/com/example/s198541_s198611_mappe2/ChangeMessageForContactsActivity.java
+++ app/src/main/java/com/example/s198541_s198611_mappe2/ChangeMessageForContactsActivity.java
@@ -31,32 +31,26 @@ public class ChangeMessageForContactsActivity extends AppCompatActivity{
         // Getting ids (for the persons chosen in ChooseContactsFragment) from the intent-data sent
         // from ChooseContactsActivity:
         ArrayList<Integer> chosenIds = getIntent().getIntegerArrayListExtra("IDS");
-        TextView textviewChosenContacts = (TextView) findViewById(R.id.text_view_for_showing_chosen_contacts);
+        TextView textViewChosenContacts = (TextView) findViewById(R.id.text_view_for_showing_chosen_contacts);
 
         if(chosenIds != null) {
-
             // Then filling chosenPersons (ArrayList) with the Person objects behind the ids:
+            // And at the same time making a string with all the contacts' names (to show to the user):
+
+            String outString = "";
 
             for(int i = 0; i < chosenIds.size(); i++) {
                 Person p = dbHandler.getPerson(chosenIds.get(i));
                 chosenPersons.add(p);
 
-
-            }
-
-            // Later (not necessarily here) we can go through chosenPersons (ArrayList) like this:
-            if(!chosenPersons.isEmpty()) {
-                String outstring ="";
-                for(int i = 0; i < chosenPersons.size(); i++) {
-                    Person p = chosenPersons.get(i);
-                    // Getting the name of the person in the log (remove later):
-                   // Log.d("NAVN: ", p.getName());
-                    outstring+= p.getName()+", ";
+                if(i == 0)
+                    outString += p.getName();
+                else {
+                    outString +=  ", " + p.getName();
                 }
-                textviewChosenContacts.setText(outstring);
-
             }
 
+            textViewChosenContacts.setText(outString);
         }
     }
 
@@ -65,57 +59,38 @@ public class ChangeMessageForContactsActivity extends AppCompatActivity{
         /*  Insert method to open select contact activity */
         Intent i = new Intent(this, ChooseContactsActivity.class);
         startActivity(i);
+        finish();
     }
 
     // OnClick save-button:
     public void saveMessageForContacts(View v) {
-        EditText edit = (EditText)findViewById(R.id.edit_message);
+        EditText edit = (EditText) findViewById(R.id.edit_message);
         String newMessage = edit.getText().toString();
+
         if(!newMessage.matches(".+")) {
             edit.setError(getString(R.string.message_error_message));
             return;
         }
-        if(!chosenPersons.isEmpty() && newMessage != "")
-        {
-            for(int i = 0; i < chosenPersons.size(); i++) {
-                Person p = chosenPersons.get(i);
-                p.setMessage(newMessage);
-                dbHandler.updatePerson(p);
-            }
-            Toast.makeText(this, getString(R.string.messaged_changed), Toast.LENGTH_SHORT).show();
-            finish();
 
+        if(chosenPersons.size() == 0) {
+            TextView textView = (TextView) findViewById(R.id.text_view_for_showing_chosen_contacts);
+            textView.setError(getString(R.string.no_contacts_chosen_error_message));
+            Toast.makeText(this, getString(R.string.no_contacts_chosen_error_message), Toast.LENGTH_SHORT).show();
+            return;
         }
 
+        for(int i = 0; i < chosenPersons.size(); i++) {
+            Person p = chosenPersons.get(i);
+            p.setMessage(newMessage);
+            dbHandler.updatePerson(p);
+        }
 
-
-
+        Toast.makeText(this, getString(R.string.message_changed), Toast.LENGTH_SHORT).show();
+        finish();
     }
 
     // OnClick cancel-button:
     public void cancel(View v) {
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_birthday_service_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-     /*   if (id == R.id.action_settings) {
-            return true;
-        }
-*/
-        return super.onOptionsItemSelected(item);
     }
 }
