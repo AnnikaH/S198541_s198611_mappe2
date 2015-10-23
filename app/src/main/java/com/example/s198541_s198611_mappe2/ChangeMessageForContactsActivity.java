bd65cb033006dd0e0cd4e96d1082e8a5545e20cb
KidaSacheil <Kassandra994@live.no>
2015-10-23 02:23:28 +0200

    Added feedback message on changed message

--- app/src/main/java/com/example/s198541_s198611_mappe2/ChangeMessageForContactsActivity.java
+++ app/src/main/java/com/example/s198541_s198611_mappe2/ChangeMessageForContactsActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -81,8 +82,12 @@ public class ChangeMessageForContactsActivity extends AppCompatActivity{
                 p.setMessage(newMessage);
                 dbHandler.updatePerson(p);
             }
+            Toast.makeText(this, getString(R.string.messaged_changed), Toast.LENGTH_SHORT).show();
+            finish();
 
         }
+
+
 
 
     }
